bf9091ceba3a4876dacd4f7555ee0e68eb14eae3
Maria-fatimaa <fatima.moreira.0112@gmail.com>
2021-03-23 23:56:05 -0300

    editando produtos

--- Views/produto.php
+++ Views/produto.php
@@ -1,1 +1,30 @@
-<h1>Essa será a página de produtos!</h1>
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+    <img src="./Midias/iphone.jpeg" class="d-block w-100 img-fluid"  alt="Erro de reenderização">
+    </div>
+    <div class="col-sm">
+    <img src="./Midias/fone-preto.jpeg" class="d-block w-100 img-fluid"  alt="Erro de reenderização">
+    </div>
+   
+    <div class="col-sm">
+    <img src="./Midias/teclado2.jpeg" class="d-block w-100 img-fluid"  alt="Erro de reenderização">
+    </div>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+    </div>
+   
+    </div>
+    <div class="col-sm">
+    
+    </div>
+    <div class="col-sm">
+    
+    </div>
+  </div>
+</div>
