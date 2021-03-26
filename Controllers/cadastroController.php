2699fa161a85b24f5e75072c9db0fb02ee223130
jose-ewerton <jose-ewerton@escolar.ifrn.edu.br>
2021-03-26 16:04:17 -0300

    Att professor

--- Controllers/cadastroController.php
+++ Controllers/cadastroController.php
@@ -3,8 +3,6 @@
 Class cadastroController extends Controller{ //unico arquivo obrigatorio ter uma funcao chamada index, tbm pela regra criada nas rotas
     public function index()
     {
-        $u = new Usuarios();
-        $info = $u->setUsuarios();
         $this->carregarTemplate('cadastro');
     }
 
--- Views/cadastro.php
+++ Views/cadastro.php
@@ -1,8 +1,15 @@
 
 <!-- Aqui que irão implementar a view de formulário cadastro -->
 
+<form id="form"  method="POST">
+        <input type="text" name="nome" id="nome" placeholder="Digite seu nome" required \>
+        <input type="text" name="email" id="email" placeholder="Digite seu e-mail" required \>
+        <input type="submit" name="acao" value="Enviar" />
+</form>
+
 <?php
 
-echo "Aqui";
+    $u = new Usuarios();
+    $info = $u->setUsuarios();
 
 ?>
