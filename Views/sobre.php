e1c85c40108403d79c12a1c6ba196aa53f3a2706
Ângela Nune <angela.medeiros560@gmail.com>
2021-03-26 19:58:08 -0300

    dando inicio a "sobre"

--- Views/sobre.php
+++ Views/sobre.php
@@ -1,1 +1,32 @@
-<h1>Esse será o conteúdo da página sobre!</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sobre</title>
+</head>
+<body>
+       <div id="Interface">
+           <hgroup id="PaginaInicial">
+                <h1> SOMOS QUATRO </h1>
+				<h2>Quem somos nós?</h2>
+			</hgroup>
+			<p>Alunas do IFRN, curso Técnico Integrado em Informática.</p>
+			</br>
+			<hgroup id=PaginaInicial>
+				<h2>por quê escolhemos fazer esse site?</h2>
+			</hgroup>
+			<p> A ideia surgiu .........</p>
+			</br>
+			<img id=  >
+            <img id=  >
+            <img id=  >
+            <img id=  >
+			<p>-- Quarteto: Ângela Nunes, Fátima Moreira, Jezana Hadassa e Sthefany Maniele</p>
+			</br>
+			</hgroup>
+		</div>
+
+
+</body>
+</html>
