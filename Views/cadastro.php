33a5f383cb2a100a83740909c23f06a0ab1f746c
jose-ewerton <jose-ewerton@escolar.ifrn.edu.br>
2021-04-01 13:12:55 -0300

    Update cadastro.php

--- Views/cadastro.php
+++ Views/cadastro.php
@@ -1,19 +1,43 @@
 
-<form id="form"  method="POST">
-  <div class="form-group">
-    <label for="exampleInputEmail1">Endereço de email</label>
-    <input type="text" class="form-control" id="nome" name="nome" aria-describedby="emailHelp" placeholder="Seu nome" required>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Endereço de email</label>
-    <input type="text" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Seu email" required>
-  </div>  
-  <button type="submit" name="acao" class="btn btn-primary">Enviar</button>
-</form>
+		<div class="container">
+			<div class="row main">
+				<div class="panel-heading">
+	               <div class="panel-title text-center">
+	               		<h1 class="title"> Cadastre-se </h1>
+	               		<hr />
+	               	</div>
+	            </div> 
+				<div class="main-login main-center">
+					<form class="form-horizontal" method="POST">
+						
+						<div class="form-group">
+							<label for="name" class="cols-sm-2 control-label"> Seu Name</label>
+							<div class="cols-sm-10">
+								<div class="input-group">
+									<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+									<input type="text" class="form-control" name="nome" id="nome"  placeholder=""/>
+								</div>
+							</div>
+						</div>
 
-<?php
+						<div class="form-group">
+							<label for="email" class="cols-sm-2 control-label"> Seu Email</label>
+							<div class="cols-sm-10">
+								<div class="input-group">
+									<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+									<input type="text" class="form-control" name="email" id="email"  placeholder=""/>
+								</div>
+							</div>
+						</div>
 
-    $u = new Usuarios();
-    $info = $u->setUsuarios();
+						<div class="form-group ">
+							<button type="button" name="acao" class="btn btn-primary btn-lg btn-block login-button"> Registre-se </button>
+						</div>
+					</form>
 
-?>
+        <?php
+
+            $u = new Usuarios();
+            $info = $u->setUsuarios();
+
+    ?>
