2b83112f415d563855d3acbac0949967f995a082
jose-ewerton <jose-ewerton@escolar.ifrn.edu.br>
2021-04-02 06:47:05 -0300

    Update do conteiner para centralizar o conteúdo

--- Views/cadastro.php
+++ Views/cadastro.php
@@ -1,16 +1,18 @@
-<form id="form" method="POST">
-  <div class="form-group">
-    <label for="email">Endereço de e-mail:</label>
-    <input type="email" class="form-control" name="email" id="email" aria-describedby="email" placeholder="">
-   
-  </div>
-  <div class="form-group">
-    <label for="nome"> Seu Nome:</label>
-    <input type="nome" class="form-control" name="nome" id="nome" placeholder="">
-  </div>
-  
-  <input type="submit" class="btn btn-primary" name= "acao" value="Enviar"/>
-</form>
+<div class="mydiv">
+  <form id="form" method="POST">
+    <div class="form-group">
+      <label for="email">Endereço de e-mail:</label>
+      <input type="email" class="form-control" name="email" id="email" aria-describedby="email" placeholder="">
+    
+    </div>
+    <div class="form-group">
+      <label for="nome"> Seu Nome:</label>
+      <input type="nome" class="form-control" name="nome" id="nome" placeholder="">
+    </div>
+    
+    <input type="submit" class="btn btn-primary" name= "acao" value="Enviar"/>
+  </form>
+</div>
 
 <?php
 
